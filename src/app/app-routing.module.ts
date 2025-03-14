902560c7b969ef4118cebc8271a54f4b7ea8fbcc
Wilmer Winkler <wilmer.winkler@se.experis.com>
2025-03-14 15:12:45 +0100

    Login and Sign up pages complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login-page/login/login.component';
+import { SignupComponent } from './sign-up-page/add/add.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
