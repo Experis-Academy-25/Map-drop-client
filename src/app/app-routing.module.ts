d30c87ebac9c396cfa5a5e10c5684a6f052977ac
Wilmer Winkler <wilmer.winkler@se.experis.com>
2025-03-17 12:54:32 +0100

    Home page first iteration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login-page/login/login.component';
 import { SignupComponent } from './sign-up-page/add/add.component';
+import { ViewComponent } from './home-page/view/view.component';
+
+
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: 'home', component: ViewComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
