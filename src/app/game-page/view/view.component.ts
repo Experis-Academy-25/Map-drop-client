03d49de1eccdf90f69edb43b0be451d2431dc94d
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-03-20 16:28:34 +0100

    Add basic streetview map on game page
    Co-Authored-By: Frida Anselin <102158306+anselin-thefirst@users.noreply.github.com>

--- src/app/game-page/view/view.component.ts
+++ src/app/game-page/view/view.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Loader } from '@googlemaps/js-api-loader';
+import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-view',
   standalone: false,
   templateUrl: './view.component.html',
-  styleUrls: ['./view.component.css']
+  styleUrls: ['./view.component.css'],
 })
 export class GameViewComponent {
   points: number = 10; // Example points value
@@ -14,7 +16,12 @@ export class GameViewComponent {
   condition: boolean = true; // Example condition
   showModal: boolean = false; // Control the visibility of the modal
 
-  hints: { text: string, link: string }[] = [
+  loader = new Loader({
+    apiKey: environment.api_key,
+    version: 'weekly',
+  });
+
+  hints: { text: string; link: string }[] = [
     { text: 'Hint 1', link: 'https://example.com/hint1' },
     { text: 'Hint 2', link: 'https://example.com/hint2' },
     { text: 'Hint 3', link: 'https://example.com/hint3' },
@@ -24,10 +31,43 @@ export class GameViewComponent {
     { text: 'Hint 7', link: 'https://example.com/hint7' },
     { text: 'Hint 8', link: 'https://example.com/hint8' },
     { text: 'Hint 9', link: 'https://example.com/hint9' },
-    { text: 'Hint 10', link: 'https://example.com/hint10' }
+    { text: 'Hint 10', link: 'https://example.com/hint10' },
   ];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.initMap();
+  }
+
+  initMap(): void {
+    let panorama: any;
+
+    const astorPlace = { lat: 40.729884, lng: -73.990988 };
+    this.loader
+      .importLibrary('streetView')
+      .then(({ StreetViewPanorama }) => {
+        panorama = new StreetViewPanorama(
+          document.getElementById('map') as HTMLElement,
+          {
+            position: astorPlace,
+            zoom: 18,
+            addressControl: false,
+          }
+        );
+
+        panorama.setPosition(astorPlace);
+        panorama.setPov(
+          /** @type {google.maps.StreetViewPov} */ {
+            heading: 265,
+            pitch: 0,
+          }
+        );
+        panorama.setAddressControlOptions(false);
+        panorama.setVisible(true);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 
   nextHint() {
     if (this.currentHintIndex < this.hints.length - 1) {
